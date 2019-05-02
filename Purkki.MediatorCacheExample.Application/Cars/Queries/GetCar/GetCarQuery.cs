c5e04b1251d56f0078eafe85707993522f9a74b3
Henri Nieminen <henri@purkki.fi>
2019-05-02 21:43:59 +0300

    Add cacheable single get query for cars

---
+++ Purkki.MediatorCacheExample.Application/Cars/Queries/GetCar/GetCarQuery.cs
@@ -0,0 +1,10 @@
+﻿using Purkki.MediatorCacheExample.Application.Infrastructure;
+using Purkki.MediatorCacheExample.Database.Entities;
+
+namespace Purkki.MediatorCacheExample.Application.Cars.Queries.GetCar
+{
+	public class GetCarQuery : ICacheableQuery<Car>
+	{
+		public int Id { get; set; }
+	}
+}
---
+++ Purkki.MediatorCacheExample.Application/Cars/Queries/GetCar/GetCarQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Purkki.MediatorCacheExample.Application.Infrastructure.Exceptions;
+using Purkki.MediatorCacheExample.Database;
+using Purkki.MediatorCacheExample.Database.Entities;
+
+namespace Purkki.MediatorCacheExample.Application.Cars.Queries.GetCar
+{
+	public class GetCarQueryHandler : IRequestHandler<GetCarQuery, Car>
+	{
+		private readonly ExampleContext _context;
+
+		public GetCarQueryHandler(ExampleContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<Car> Handle(GetCarQuery request, CancellationToken cancellationToken)
+		{
+			var car = await _context.Cars.FirstOrDefaultAsync(c => c.Id == request.Id, cancellationToken);
+			if (car == null)
+			{
+				throw new NotFoundException(nameof(car), request.Id.ToString());
+			}
+
+			return car;
+		}
+	}
+}
