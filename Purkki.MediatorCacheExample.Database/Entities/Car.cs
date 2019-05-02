b970cf26cfe5c7bda6d7d8046a654bc1b382e5ca
Henri Nieminen <henri@purkki.fi>
2019-05-02 21:17:11 +0300

    Add database project

---
+++ Purkki.MediatorCacheExample.Database/Entities/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace Purkki.MediatorCacheExample.Database.Entities
+{
+	public class Car
+	{
+		public int Id { get; set; }
+		public string Make { get; set; }
+		public string Model { get; set; }
+	}
+}
---
+++ Purkki.MediatorCacheExample.Database/ExampleContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Purkki.MediatorCacheExample.Database.Entities;
+
+namespace Purkki.MediatorCacheExample.Database
+{
+	public class ExampleContext : DbContext
+	{
+		public ExampleContext(DbContextOptions<ExampleContext> options) : base(options) { }
+
+		public DbSet<Car> Cars { get; set; }
+	}
+}
