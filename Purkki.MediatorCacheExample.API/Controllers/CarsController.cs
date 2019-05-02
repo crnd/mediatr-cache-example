ba2014219c4ca9c98079fc4054da3ee28f881b68
Henri Nieminen <henri@purkki.fi>
2019-05-02 22:20:43 +0300

    Add API controller for managing cars

---
+++ Purkki.MediatorCacheExample.API/Controllers/CarsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Purkki.MediatorCacheExample.Application.Cars.Commands.CreateCar;
+using Purkki.MediatorCacheExample.Application.Cars.Commands.DeleteCar;
+using Purkki.MediatorCacheExample.Application.Cars.Queries.GetCar;
+using Purkki.MediatorCacheExample.Application.Cars.Queries.GetCars;
+using Purkki.MediatorCacheExample.Database.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Purkki.MediatorCacheExample.API.Controllers
+{
+	public class CarsController : MediatorControllerBase
+	{
+		private const string GetCarRouteName = "GetCar";
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Car>>> Get()
+		{
+			return Ok(await Mediator.Send(new GetCarsQuery()));
+		}
+
+		[HttpGet("{id}", Name = GetCarRouteName)]
+		public async Task<ActionResult<Car>> Get(int id)
+		{
+			return Ok(await Mediator.Send(new GetCarQuery { Id = id }));
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Create(CreateCarCommand command)
+		{
+			var id = await Mediator.Send(command);
+			return CreatedAtRoute(GetCarRouteName, new { id }, null);
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<ActionResult> Delete(int id)
+		{
+			await Mediator.Send(new DeleteCarCommand { Id = id });
+			return NoContent();
+		}
+	}
+}
---
+++ Purkki.MediatorCacheExample.API/Controllers/MediatorControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Purkki.MediatorCacheExample.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public abstract class MediatorControllerBase : ControllerBase
+	{
+		private IMediator _mediator;
+
+		protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
+	}
+}
