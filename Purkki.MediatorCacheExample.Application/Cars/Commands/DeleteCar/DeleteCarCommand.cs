9156b80961a3bfb0fb817714e474d6050b16ac25
Henri Nieminen <henri@purkki.fi>
2019-05-02 21:53:02 +0300

    Add command for deleting cars

---
+++ Purkki.MediatorCacheExample.Application/Cars/Commands/DeleteCar/DeleteCarCommand.cs
@@ -0,0 +1,9 @@
+﻿using Purkki.MediatorCacheExample.Application.Infrastructure;
+
+namespace Purkki.MediatorCacheExample.Application.Cars.Commands.DeleteCar
+{
+	public class DeleteCarCommand : ICommand
+	{
+		public int Id { get; set; }
+	}
+}
---
+++ Purkki.MediatorCacheExample.Application/Cars/Commands/DeleteCar/DeleteCarCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Purkki.MediatorCacheExample.Application.Infrastructure.Exceptions;
+using Purkki.MediatorCacheExample.Database;
+
+namespace Purkki.MediatorCacheExample.Application.Cars.Commands.DeleteCar
+{
+	public class DeleteCarCommandHandler : IRequestHandler<DeleteCarCommand>
+	{
+		private readonly ExampleContext _context;
+
+		public DeleteCarCommandHandler(ExampleContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<Unit> Handle(DeleteCarCommand request, CancellationToken cancellationToken)
+		{
+			var car = await _context.Cars.FirstOrDefaultAsync(c => c.Id == request.Id, cancellationToken);
+			if (car == null)
+			{
+				throw new NotFoundException(nameof(car), request.Id.ToString());
+			}
+
+			_context.Cars.Remove(car);
+			await _context.SaveChangesAsync(cancellationToken);
+
+			return Unit.Value;
+		}
+	}
+}
