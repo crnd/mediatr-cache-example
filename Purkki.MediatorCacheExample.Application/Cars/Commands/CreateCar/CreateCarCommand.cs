dd811f99250f959c5abe73ace44e728e2a95f40d
Henri Nieminen <henri@purkki.fi>
2019-05-02 21:49:42 +0300

    Add command for creating cars

---
+++ Purkki.MediatorCacheExample.Application/Cars/Commands/CreateCar/CreateCarCommand.cs
@@ -0,0 +1,10 @@
+﻿using Purkki.MediatorCacheExample.Application.Infrastructure;
+
+namespace Purkki.MediatorCacheExample.Application.Cars.Commands.CreateCar
+{
+	public class CreateCarCommand : ICommand<int>
+	{
+		public string Make { get; set; }
+		public string Model { get; set; }
+	}
+}
---
+++ Purkki.MediatorCacheExample.Application/Cars/Commands/CreateCar/CreateCarCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Purkki.MediatorCacheExample.Database;
+using Purkki.MediatorCacheExample.Database.Entities;
+
+namespace Purkki.MediatorCacheExample.Application.Cars.Commands.CreateCar
+{
+	public class CreateCarCommandHandler : IRequestHandler<CreateCarCommand, int>
+	{
+		private readonly ExampleContext _context;
+
+		public CreateCarCommandHandler(ExampleContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<int> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+		{
+			var car = new Car
+			{
+				Make = request.Make,
+				Model = request.Model
+			};
+
+			_context.Cars.Add(car);
+			await _context.SaveChangesAsync(cancellationToken);
+
+			return car.Id;
+		}
+	}
+}
