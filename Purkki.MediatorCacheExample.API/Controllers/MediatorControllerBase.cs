4f1ee915b84caffa1fcc295cc39f56525bc7842e
Henri Nieminen <henri@purkki.fi>
2020-07-07 23:10:18 +0200

    Use null-coalescing assignment in base controller

--- Purkki.MediatorCacheExample.API/Controllers/MediatorControllerBase.cs
+++ Purkki.MediatorCacheExample.API/Controllers/MediatorControllerBase.cs
@@ -10,6 +10,6 @@ namespace Purkki.MediatorCacheExample.API.Controllers
 	{
 		private IMediator _mediator;
 
-		protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
+		protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
 	}
 }
