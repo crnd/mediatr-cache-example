d37f9a7b4ae9593ef9ab3f3a003c457f05f9c732
Henri Nieminen <henri@purkki.fi>
2020-07-08 00:15:35 +0200

    Rename controller actions

--- Purkki.MediatorCacheExample.API/Controllers/CarsController.cs
+++ Purkki.MediatorCacheExample.API/Controllers/CarsController.cs
@@ -12,26 +12,26 @@ namespace Purkki.MediatorCacheExample.API.Controllers
 	public class CarsController : MediatorControllerBase
 	{
 		[HttpGet]
-		public async Task<ActionResult<List<Car>>> Get()
+		public async Task<ActionResult<List<Car>>> GetCars()
 		{
 			return await Mediator.Send(new GetCarsQuery());
 		}
 
 		[HttpGet("{id}")]
-		public async Task<ActionResult<Car>> Get(int id)
+		public async Task<ActionResult<Car>> GetCar(int id)
 		{
 			return await Mediator.Send(new GetCarQuery { Id = id });
 		}
 
 		[HttpPost]
-		public async Task<ActionResult> Create(CreateCarCommand command)
+		public async Task<ActionResult> CreateCar(CreateCarCommand command)
 		{
 			var car = await Mediator.Send(command);
-			return CreatedAtAction(nameof(Get), new { car.Id }, car);
+			return CreatedAtAction(nameof(GetCar), new { car.Id }, car);
 		}
 
 		[HttpDelete("{id}")]
-		public async Task<ActionResult> Delete(int id)
+		public async Task<ActionResult> DeleteCar(int id)
 		{
 			await Mediator.Send(new DeleteCarCommand { Id = id });
 			return NoContent();
